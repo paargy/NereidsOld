1909f496331a856fb2fbc665186c8a23c1123edf
evgenia2002 <79972107+evgenia2002@users.noreply.github.com>
2021-11-17 21:12:09 +0200

    Update Score.java
    updated class score

--- Score.java
+++ Score.java
@@ -1,11 +1,16 @@
 public class Score{
-  static int score = 5;
-  public static int DificultyOvercome(){
-    score += 2;
-    return score;
-  }
-  public static int minusOne(){ // used every time characteristic does not exist
-    score -= 1;
-    return score;
-  }
+  
+	static int score = 5;
+
+	public static void DificultyOvercome(){
+		score += 2;
+	}
+
+	public static void minusOne(){  // used every time characteristic does not exist
+		score -= 1;
+	}
+
+	public static void showScore(){
+		System.out.println("You won!! Your score was: " + score);
+	}
 }
